2835b2c178ecd5dfe29c0256bfcbf451bf935942
xzg_1217 <1359859140@qq.com>
2025-10-12 22:02:39 +0800

    初始化

---
+++ src/views/home/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+    <!-- 顶部导航条 -->
+    <div class="home-header">
+      <div class="home-header-left">
+        <!-- Logo -->
+        <ElImage src="/src/assets/img/logo.png" class="home-header-left-logo" />
+        <!-- 导航菜单 -->
+        <div class="home-header-left-menu">
+          <div
+            class="home-header-left-menu-item flex"
+            v-for="(item, index) in menus"
+            :key="item.path"
+          >
+            <i class="iconfont-sys" :class="item.icon"></i>
+            <p class="name">{{ item.name }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="home-header-right"> 右 <i class="iconfont-sys">&#xe7f1;</i> </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const menus = ref([
+    {
+      name: '聊天',
+      path: '/'
+    },
+    {
+      name: 'wisebase',
+      path: '/wisebase',
+      icon: 'iconsys-xia2',
+      hoverIcon: 'iconsys-shang2'
+    },
+    {
+      name: '工具',
+      path: '/wisebase/agent',
+      icon: 'iconsys-xia2',
+      hoverIcon: 'iconsys-shang2'
+    },
+    {
+      name: '扩展程序',
+      path: '/setting'
+    },
+    {
+      name: '应用',
+      path: '/setting',
+      icon: 'iconsys-xia2',
+      hoverIcon: 'iconsys-shang2'
+    },
+    {
+      name: '价格',
+      path: '/setting'
+    }
+  ])
+</script>
+
+<style lang="scss" scoped>
+  @use './index';
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,143 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import viteCompression from 'vite-plugin-compression'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import ElementPlus from 'unplugin-element-plus/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// import { visualizer } from 'rollup-plugin-visualizer'
+
+export default ({ mode }: { mode: string }) => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  const { VITE_VERSION, VITE_PORT, VITE_BASE_URL, VITE_API_URL, VITE_API_PROXY_URL } = env
+
+  console.log(`🚀 API_URL = ${VITE_API_URL}`)
+  console.log(`🚀 VERSION = ${VITE_VERSION}`)
+
+  return defineConfig({
+    define: {
+      __APP_VERSION__: JSON.stringify(VITE_VERSION)
+    },
+    base: VITE_BASE_URL,
+    server: {
+      port: Number(VITE_PORT),
+      proxy: {
+        '/api': {
+          target: VITE_API_PROXY_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      },
+      host: true
+    },
+    // 路径别名
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@views': resolvePath('src/views'),
+        '@imgs': resolvePath('src/assets/img'),
+        '@icons': resolvePath('src/assets/icons'),
+        '@utils': resolvePath('src/utils'),
+        '@stores': resolvePath('src/store'),
+        '@plugins': resolvePath('src/plugins'),
+        '@styles': resolvePath('src/assets/styles')
+      }
+    },
+    build: {
+      target: 'es2015',
+      outDir: 'dist',
+      chunkSizeWarningLimit: 2000,
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          // 生产环境去除 console
+          drop_console: true,
+          // 生产环境去除 debugger
+          drop_debugger: true
+        }
+      },
+      dynamicImportVarsOptions: {
+        warnOnError: true,
+        exclude: [],
+        include: ['src/views/**/*.vue']
+      }
+    },
+    plugins: [
+      vue(),
+      // 自动按需导入 API
+      AutoImport({
+        imports: ['vue', 'vue-router', '@vueuse/core', 'pinia'],
+        dts: 'src/types/auto-imports.d.ts',
+        eslintrc: {
+          enabled: true,
+          filepath: './.auto-import.json'
+        },
+        resolvers: [ElementPlusResolver()]
+      }),
+      // 自动按需导入组件
+      Components({
+        dts: 'src/types/components.d.ts',
+        resolvers: [ElementPlusResolver()]
+      }),
+      // 按需定制主题配置
+      ElementPlus({
+        useSource: true
+      }),
+      // 压缩
+      viteCompression({
+        verbose: false, // 是否在控制台输出压缩结果
+        disable: false, // 是否禁用
+        algorithm: 'gzip', // 压缩算法
+        ext: '.gz', // 压缩后的文件名后缀
+        threshold: 10240, // 只有大小大于该值的资源会被处理 10240B = 10KB
+        deleteOriginFile: false // 压缩后是否删除原文件
+      }),
+      vueDevTools()
+      // 打包分析
+      // visualizer({
+      //   open: true,
+      //   gzipSize: true,
+      //   brotliSize: true,
+      //   filename: 'dist/stats.html' // 分析图生成的文件名及路径
+      // }),
+    ],
+    // 依赖预构建
+    optimizeDeps: {
+      include: ['element-plus/es/components/*/style/css']
+    },
+    css: {
+      preprocessorOptions: {
+        // sass variable and mixin
+        scss: {
+          additionalData: `
+            @use "@styles/el-light.scss" as *; 
+            @use "@styles/variables.scss" as *; 
+            @use "@styles/mixin.scss" as *;
+          `
+        }
+      },
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove()
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
+  })
+}
+
+function resolvePath(paths: string) {
+  return path.resolve(__dirname, paths)
+}
